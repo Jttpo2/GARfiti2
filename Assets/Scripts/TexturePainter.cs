01d423376f99267ee7a1ec5caf1d013a1feea49c
John Petersson <johnpetersson@gmail.com>
2016-11-02 21:52:20 +0100

    Apparently Imput.mouseposition() is the same as first touch.position

--- Assets/Scripts/TexturePainter.cs
+++ Assets/Scripts/TexturePainter.cs
@@ -99,8 +99,9 @@ public class TexturePainter : MonoBehaviour
 	bool HitTestUVPosition (ref Vector3 uvWorldPosition)
 	{
 		RaycastHit hit;
-//		Vector3 cursorPos = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0.0f);
-		Vector3 cursorPos = getInputPosition ();
+		// Input.mousePosition() works for one finger touch input as well
+		Vector3 cursorPos = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0.0f);
+//		Vector3 cursorPos = getInputPosition ();
 		Ray cursorRay = sceneCamera.ScreenPointToRay (cursorPos);
 		if (Physics.Raycast (cursorRay, out hit, 200)) {
 			MeshCollider meshCollider = hit.collider as MeshCollider;
@@ -118,27 +119,27 @@ public class TexturePainter : MonoBehaviour
 		
 	}
 
-	private Vector3 getInputPosition ()
-	{
-		Vector3 touchPosition = getTouchPosition ();
-		if (touchPosition != Vector3.zero) {
-			return touchPosition;
-		} else {
-			return new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0.0f);	
-		}
-	}
-
-	private Vector3 getTouchPosition ()
-	{
-		foreach (Touch touch in Input.touches) {
-			if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary) {
-				// Return first touch found
-				return new Vector3 (touch.position.x, touch.position.y, 0.0f);
-			} 
-		}
-		return Vector3.zero;
-
-	}
+	//	private Vector3 getInputPosition ()
+	//	{
+	//		Vector3 touchPosition = getTouchPosition ();
+	//		if (touchPosition != Vector3.zero) {
+	//			return touchPosition;
+	//		} else {
+	//			return new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0.0f);
+	//		}
+	//	}
+	//
+	//	private Vector3 getTouchPosition ()
+	//	{
+	//		foreach (Touch touch in Input.touches) {
+	//			if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary) {
+	//				// Return first touch found
+	//				return new Vector3 (touch.position.x, touch.position.y, 0.0f);
+	//			}
+	//		}
+	//		return Vector3.zero;
+	//
+	//	}
 
 
 
