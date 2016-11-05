69bd97037b5b5bb4baab0188be0a4e6509903702
John Petersson <johnpetersson@gmail.com>
2016-11-05 13:56:58 +0100

    Color picker visible

--- Assets/Scripts/ExampleColorReceiver.cs
+++ Assets/Scripts/ExampleColorReceiver.cs
@@ -1,23 +1,24 @@
 using UnityEngine;
 
-public class ExampleColorReceiver : MonoBehaviour {
+public class ExampleColorReceiver : MonoBehaviour
+{
 	
-    Color color;
+	Color color;
 
-	void OnColorChange(HSBColor color) 
+	void OnColorChange (HSBColor color)
 	{
-        this.color = color.ToColor();
+		this.color = color.ToColor ();
 	}
 
-    void OnGUI()
-    {
-		var r = Camera.main.pixelRect;
-		var rect = new Rect(r.center.x + r.height / 6 + 50, r.center.y, 100, 100);
-		GUI.Label (rect, "#" + ToHex(color.r) + ToHex(color.g) + ToHex(color.b));	
-    }
-
-	string ToHex(float n)
+	void OnGUI ()
 	{
-		return ((int)(n * 255)).ToString("X").PadLeft(2, '0');
+//		var r = Camera.main.pixelRect;
+//		var rect = new Rect(r.center.x + r.height / 6 + 50, r.center.y, 100, 100);
+//		GUI.Label (rect, "#" + ToHex(color.r) + ToHex(color.g) + ToHex(color.b));	
+	}
+
+	string ToHex (float n)
+	{
+		return ((int)(n * 255)).ToString ("X").PadLeft (2, '0');
 	}
 }
